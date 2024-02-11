9ca01d8e601850bd3971697a093d0dc9fb62a30f
Thilanka Bowala <thilankabowala@gmail.com>
2024-02-11 14:14:30 +0530

    Main class change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import com.example.helloworld.Cycle;
+import com.example.helloworld.HelloWorld;
 
 public class Main {
     private static final Cycle c = new Cycle();
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Cycle c = new Cycle();
+        HelloWorld.main(null);
     }
 }
